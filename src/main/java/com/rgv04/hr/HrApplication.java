a09cfd453691ec308b51cde2c398badd965f2d2d
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-06-11 10:32:25 -0300

    rename application file

--- src/main/java/com/rgv04/hr/HrApplication.java
+++ src/main/java/com/rgv04/hr/HrApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiarioObrasApplication {
+public class HrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiarioObrasApplication.class, args);
+		SpringApplication.run(HrApplication.class, args);
 	}
 
 }
