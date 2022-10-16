9fd46f5eebbea8429c27a1e6791d20b137393ea9
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-10-16 11:42:32 -0300

    fix: alter user.getName() to user.getUsername() payload subject jwt

--- src/main/java/com/rgv04/hr/security/service/UserServiceImpl.java
+++ src/main/java/com/rgv04/hr/security/service/UserServiceImpl.java
@@ -79,7 +79,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         user.getRoles().forEach(role -> {
             authorities.add(new SimpleGrantedAuthority(role.getName()));
         });
-        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), authorities);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 
 }
