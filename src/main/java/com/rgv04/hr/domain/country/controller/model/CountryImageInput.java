c4a1629a71ffacaa3d0ab044ea529938a540a065
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-10-30 16:08:38 -0300

    feat: create validation fileSize e contenttype

--- src/main/java/com/rgv04/hr/domain/country/controller/model/CountryImageInput.java
+++ src/main/java/com/rgv04/hr/domain/country/controller/model/CountryImageInput.java
@@ -3,7 +3,11 @@ package com.rgv04.hr.domain.country.controller.model;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.rgv04.hr.core.validation.FileContentType;
+import com.rgv04.hr.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +17,11 @@ import lombok.Setter;
 public class CountryImageInput {
 
 	@NotNull
-	//@FileSize(max = "500KB")
-	//@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
+	@FileSize(max = "500KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile file;
-	
+
 	@NotBlank
 	private String description;
-    
+
 }
