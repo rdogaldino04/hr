961a8f465fb9263c4472f8f3cf04d6e5d72d42a3
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-10-30 20:49:26 -0300

    refector: delete endpoint post api/regions

--- src/main/java/com/rgv04/hr/domain/region/Region.java
+++ src/main/java/com/rgv04/hr/domain/region/Region.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -27,7 +25,6 @@ import lombok.ToString;
 public class Region {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "REGION_ID")
 	private Long id;
 
--- src/main/java/com/rgv04/hr/domain/region/RegionController.java
+++ src/main/java/com/rgv04/hr/domain/region/RegionController.java
@@ -2,15 +2,11 @@ package com.rgv04.hr.domain.region;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,14 +28,6 @@ public class RegionController {
 	@Autowired
 	private RegionWithCountryAssembler regionWithCountryAssembler;
 
-	@PostMapping
-	public ResponseEntity<RegionOutput> save(@RequestBody @Valid RegionInput regionInput) {
-		Region region = toEntity(regionInput);
-		region = regionService.save(region);
-		RegionOutput regionOutput = toOutput(region);
-		return ResponseEntity.ok(regionOutput);
-	}
-
 	@GetMapping
 	public ResponseEntity<CollectionModel<RegionModel>> listByFilter(RegionFilter filter) {
 		List<Region> listEntity = this.regionService.listByFilter(filter);
@@ -58,19 +46,6 @@ public class RegionController {
 	public ResponseEntity<RegionWithCountryModel> countriesByRegionId(@PathVariable Long regionId) {
 		Region regions = regionService.countriesByRegionId(regionId);		
 		return ResponseEntity.ok(regionWithCountryAssembler.toModel(regions));
-	}
-
-	private RegionOutput toOutput(Region region) {
-		RegionOutput regionOutput = new RegionOutput();
-		regionOutput.setId(region.getId());
-		regionOutput.setName(region.getName());
-		return regionOutput;
-	}
-
-	private Region toEntity(RegionInput regionInput) {
-		Region region = new Region();
-		region.setName(regionInput.getName());
-		return region;
 	}
 
 }
